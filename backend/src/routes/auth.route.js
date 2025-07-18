491a3b347bc253cd586500edb2b029e0cdba17fb
Shubham Sharma <shubhamsharma.100799@gmail.com>
2025-07-18 17:34:04 +1000

    Add update profile route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
-
+import { protectRoute } from "../middleware/auth.middleware.js"
 const router = express.Router()
 
 router.post('/signup', signup)
@@ -8,5 +8,8 @@ router.post('/signup', signup)
 router.post('/login', login)
 
 router.post('/logout', logout)
+
+// protectRoute checks if we have jwt token in cookie (Authenticated)
+router.put('/update-profile', protectRoute, updateProfile)
 
 export default router;
