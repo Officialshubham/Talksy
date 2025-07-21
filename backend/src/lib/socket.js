cca8dea838b4dc0632aeec048c2b23d84206dc76
Shubham Sharma <shubhamsharma.100799@gmail.com>
2025-07-21 21:35:46 +1000

    Change typo from Server to server

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,7 +6,7 @@ import express from 'express'
 const app = express()
 const server = http.createServer(app)
 
-const io = new Server(server, {
+const io = new server(server, {
     cors: {
         origin: ['http://localhost:5173']
     }
