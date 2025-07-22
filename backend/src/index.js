b46c96e52726a5e017f6c8da53507a876dd99b38
Shubham Sharma <shubhamsharma.100799@gmail.com>
2025-07-22 13:54:28 +1000

    Add port fallback

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import path from 'path'
 dotenv.config()
 
 const __dirname = path.resolve()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5001
 
 app.use(express.json())
 app.use(cookieParser())
@@ -25,10 +25,10 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+        res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
     })
 }
 
